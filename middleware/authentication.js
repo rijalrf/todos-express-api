14256b6f6daad0e7de7fcf06774441074418d72e
Muhamad Rijal Rifai <rifairijal97@gmail.com>
2025-09-18 22:13:55 +0700

    update

--- middleware/authentication.js
+++ middleware/authentication.js
@@ -1,14 +1,14 @@
 // auth header api key check middleware
 const auth = (req, res, next) => {
-  // const apiKey = req.header("x-api-key");
-  // if (apiKey && apiKey === process.env.API_KEY) {
-  //   next();
-  // } else {
-  //   res.status(401).json({
-  //     sucess: false,
-  //     message: "Unauthorized",
-  //   });
-  next();
+  const apiKey = req.header("x-api-key");
+  if (apiKey && apiKey === process.env.API_KEY) {
+    next();
+  } else {
+    res.status(401).json({
+      sucess: false,
+      message: "Unauthorized",
+    });
+  }
 };
 
 export default auth;
